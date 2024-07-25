b96f5665d5366317174521ffc44c7e3ce53c3628
silaghiandrei <silaghiandrei03@gmail.com>
2024-07-25 19:13:14 +0300

    Failed updates

--- app/src/main/java/com/example/alarmapp/SecondActivity.java
+++ app/src/main/java/com/example/alarmapp/SecondActivity.java
@@ -1,13 +1,20 @@
 package com.example.alarmapp;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TimePicker timePicker = findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
     }
+
 }
